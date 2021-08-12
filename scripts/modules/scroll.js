a1ed5633e5c6da8d66d4c111cdaa4555e8e321de
Victor Ambrozi <victorambrozi.dev@gmail.com>
2021-08-12 15:01:53 -0300

    anima menu e main ao  scroll

--- scripts/modules/scroll.js
+++ scripts/modules/scroll.js
@@ -2,15 +2,19 @@ export default function animationScroll() {
 
     function animationCss() {
         const imageIntro = document.querySelector('[data-anime="image-intro"]');
-        const rect = imageIntro.getBoundingClientRect();
-        const imageTop = rect.top;
+        const imageTop = imageIntro.getBoundingClientRect().top;
         const header = document.querySelector('[data-responsive="header"]');
 
         if (imageTop < 0) {
             // adiciona propriedade no elemento para aplicar a animação
             header.dataset.anime = 'show-down-menu';
+
             header.classList.add('active');
+            // estilo menu
             header.style.position = 'fixed';
+            header.style.right = '0';
+            header.style.left = '0';
+            header.style.zIndex = '99999';
 
         } else {
             header.style.position = 'static';
@@ -20,28 +24,24 @@ export default function animationScroll() {
 
     document.addEventListener("scroll", animationCss);
 
+    const sections = [...document.querySelectorAll('[data-scroll="scroll"]')];
+
+    // ativa animações na primiera section
+    sections[0].dataset.anime = sections[0].parentNode.dataset.anime;
+    sections[0].classList.add('active');
+
     function sectionTransition() {
-        const sections = [...document.querySelectorAll('main section')];
-        sections[0].classList.add('active');
-        
+
         sections.forEach((section) => {
             const height = section.getBoundingClientRect().top;
             const halfscreen = window.innerHeight * 0.5;
-            
-            if(height < halfscreen) {
+
+            if (height < halfscreen) {
+                section.dataset.anime = section.parentNode.dataset.anime;
                 section.classList.add('active');
-            } else {
-                section.classList.remove('active');
             }
-
-            console.log(height)
-
-
         })
-
     }
 
     document.addEventListener("scroll", sectionTransition);
-
-
 }
