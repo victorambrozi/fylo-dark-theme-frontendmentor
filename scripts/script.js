af413e99031ffca999de3c274950aa5320e9e25e
Victor Ambrozi <victorambrozi.dev@gmail.com>
2021-09-28 01:03:55 -0300

    corrige ortografia

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-import showMenu from './modules/responsive.js'
-import animationScroll from './modules/scroll.js'
-import initModal from './modules/modal.js'
+import showMenu from './modules/responsive.js';
+import animationScroll from './modules/scroll.js';
+import initModal from './modules/modal.js';
 
 showMenu();
 animationScroll();
